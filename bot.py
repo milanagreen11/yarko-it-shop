5dc7b49ab960a7ad221410e25408f52e14372f30
Milana <milanagreen11@gmail.com>
2025-12-23 10:41:42 +0300

    Update0000000000000000 bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 import telebot
 
 TOKEN = '8130399116:AAEB9Fd8cvUEK61rtpWa-18bB0cHFOjIEgw'
-ADMIN_ID = 5239006573
+ADMIN_ID = 52390006573
 
 bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    bot.forward_message(ADMIN_ID, message.chat.id, message.message_id)
+    bot.send_message(message.chat.id, "Спасибо за заказ! Скоро свяжусь 😊")
 
 @bot.message_handler(content_types=['web_app_data'])
 def handle_web_app_data(message):
@@ -14,11 +19,8 @@ def handle_web_app_data(message):
 def start(message):
     bot.send_message(message.chat.id, "Добро пожаловать! Нажмите кнопку внизу, чтобы открыть магазин IT-услуг 🛒")
 
-@bot.message_handler(text=['Тест'])
-def test(message):
-    bot.send_message(message.chat.id, "Питон в порядке!")
-
 print("Бот запущен и ждёт заказы!")
 bot.infinity_polling()
+
 
 
